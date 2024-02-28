9f955add06231639036ca31f4321a1bd8f14edba
Alesia_Shikurova <ales2503@yandex.by>
2024-02-28 23:38:01 +0300

    feat: add getDayName task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -49,8 +49,36 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const date1 = new Date(date);
+  date1.getDay();
+  let days = '';
+  switch (date1.getDay()) {
+    case 0:
+      days = 'Sunday';
+      break;
+    case 1:
+      days = 'Monday';
+      break;
+    case 2:
+      days = 'Tuesday';
+      break;
+    case 3:
+      days = 'Wednesday';
+      break;
+    case 4:
+      days = 'Thursday';
+      break;
+    case 5:
+      days = 'Friday';
+      break;
+    case 6:
+      days = 'Saturday';
+      break;
+    default:
+      break;
+  }
+  return days;
 }
 
 /**
